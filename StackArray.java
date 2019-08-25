15ae7395118a77ae60612f23c539c5351321e966
MANISHA KUMARI <53348962+manisha-jaiswal@users.noreply.github.com>
2019-08-25 15:05:32 +0530

    stack using array

---
+++ StackArray.java
@@ -0,0 +1,70 @@
+import  java.io.*;
+class StackArray
+{
+    static int max=10,i,top,ch,item;
+    static int a[]=new int[10];
+    StackArray()
+    {
+        top=-1;
+    }
+    public static void main(String args[])throws IOException
+    {
+        while((boolean)true)
+        {
+            System.out.println("enter \n1.Push \n2.Pop \n3.Display \n4.Exit");
+ 
+            try
+            {
+                BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                ch=Integer.parseInt(br.readLine());
+            }
+            catch(Exception e) { }
+            if(ch==4)
+                break;
+            else
+            {
+                switch(ch)
+                {
+                case 1:
+                    push();
+                    break;
+                case 2:
+                    pop();
+                    break;
+                case 3:
+                    display();
+                    break;
+                }
+            }
+        }
+    }
+    static void push()
+    {
+        if(top==max)
+            System.out.println("stack is full");
+        else
+ 
+            try
+            {
+                BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("enter the element:");
+                item=Integer.parseInt(br.readLine());
+                a[++top]=item;
+            }
+            catch(Exception e) { }
+    }
+    static void pop()
+    {
+        if(top==0)
+            System.out.println("stack is empty");
+        else
+            top--;
+        System.out.println("poped item:"+a[top+1]);
+    }
+    static void display()
+    {
+        System.out.println("elements in stack are:");
+        for(i=top; i>0; i--)
+            System.out.println(a[i]);
+    }
+}
