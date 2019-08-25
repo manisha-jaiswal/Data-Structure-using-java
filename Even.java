becb85165020a853fb5d9c6a8cc45c0f566c2293
MANISHA KUMARI <53348962+manisha-jaiswal@users.noreply.github.com>
2019-08-25 14:56:17 +0530

    Even

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Even
+{
+ public static void main(String []args)
+{
+ int n;
+Scanner obj=new Scanner(System.in);
+n=obj.nextInt();
+if(n %2==0)
+System.out.println("even no");
+else 
+System.out.println("odd no");
+}
+}
